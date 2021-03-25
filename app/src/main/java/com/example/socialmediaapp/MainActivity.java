3c261bbf8e3c7cce944dce21b753b01a8ee3f84e
omercanbaltaci <57769219+omercanbaltaci@users.noreply.github.com>
2021-03-25 19:54:55 +0300

    Landing page

--- app/src/main/java/com/example/socialmediaapp/MainActivity.java
+++ app/src/main/java/com/example/socialmediaapp/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.socialmediaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.socialmediaapp.Activities.LoginPage;
 
 public class MainActivity extends AppCompatActivity {
+
+    // XML
+    Button moveToLoginPageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        attachJavaObjectToXML();
+
+        moveToLoginPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginPage();
+            }
+        });
+    }
+
+    private void attachJavaObjectToXML() {
+        try {
+            moveToLoginPageBtn = findViewById(R.id.mainActivity_MoveBtn);
+        } catch (Exception e) {
+            Toast.makeText(this, "MainAction:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void goToLoginPage() {
+        try {
+            startActivity(new Intent(this, LoginPage.class));
+        } catch (Exception e) {
+            Toast.makeText(this, "MainActivity:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
